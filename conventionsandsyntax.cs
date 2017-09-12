80b9237d0d84c2cf5482bd21ca37b28227f92955
Unknown <lexi.hougaard@gmail.com>
2017-09-11 23:25:50 -0600

    In class Conven Syn
    Conventions and syntax file

---
+++ conventionsandsyntax.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+ 
+public class conditionals : MonoBehaviour { 
+  //Conventions and syntax 
+  public int blood = 10;
+  //each line of code needs to end with a ; 
+  //that's how it knows when to move on the next instructions
+  if(stopLight == "Red") { 
+    print("STOP! ThE lIgHt Is ReD!!"); 
+ 
+    }  
+  //{} show the beginning and end of code 
+  //keeps the situation together fotr the if statement
+     }  
+    else if (stopLight == "Yellow"){  
+    print("SlOw DoWn!!!"); 
+    } 
+// = puts a value on a variable and == compares the value
+
+  public string stopLight = "Red"; 
+  void start(){ 
+    Main(); 
+  } 
+  void Main () { 
+    if(stopLight == "Red") { 
+    print("STOP! ThE lIgHt Is ReD!!"); 
+ 
+    }  
+    else if (stopLight == "Yellow"){  
+    print("SlOw DoWn!!!"); 
+    } 
+    else if (stopLight == "Green"){  
+    print("Go!!!!"); 
+    } 
+    else{ 
+    print("I dOn'T kNoW wHaT yOu MeAn!"); 
+    } 
+ //the conventions of white space in this block of code made the code
+ //easier to understand. The organization of the code also makes it 
+ //easier to find errors.
+ 
+print("Help ME!!!");
+//quotation marks indicate text. the command print won't print the words
+//when ran because it won't recodnize the text as a string
+
+} 
+ 
+}
+
